327959f3959632e438708ba30f5e3f0b658c7145
phucnv <phuc130497@gmail.com>
2021-07-23 22:47:55 +0700

    2021/07/23 Github #5

--- src/main/java/com/shopwatch/api/controller/result/ProductResult.java
+++ src/main/java/com/shopwatch/api/controller/result/ProductResult.java
@@ -2,15 +2,20 @@ package com.shopwatch.api.controller.result;
 
 import java.util.Date;
 
+import com.shopwatch.api.entity.Brand;
+import com.shopwatch.api.entity.ColorWatch;
+
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class ProductResult {
 	private String name;
 	private int price_old;
 	private int price_new;
 	private String images;
-	private int color_id;
-	private int brand_id;
+	private ColorWatch color;
+	private Brand brand;
 	private Date create_at;
 }
--- src/main/java/com/shopwatch/api/repository/ProductRepository.java
+++ src/main/java/com/shopwatch/api/repository/ProductRepository.java
@@ -19,6 +19,6 @@ public interface ProductRepository extends JpaRepository<Product, Integer>{
 	@Query(value = "select * from product p where p.status = ?1", nativeQuery = true)
 	List<Product> customSelectAllProduct(boolean status);
 	
-	@Query(value = "select name, price_old, price_new, images, color_id, brand_id, create_at from product p where p.status = ?1", nativeQuery = true)
+	@Query("select new com.shopwatch.api.controller.result.ProductResult(p.name, p.price_old, p.price_new, p.images, p.colorWatch, p.brand, p.create_at) from Product p where p.status = ?1")
 	List<ProductResult> selectAllProductResult(boolean status);
 }
